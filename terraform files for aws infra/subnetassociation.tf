ec945294cf8395c0409d24c55105255d696973a7
saicharan-clan <ssaicharan0990@gmail.com>
2025-03-17 18:16:14 +0530

    Create subnetassociation.tf

---
+++ terraform files for aws infra/subnetassociation.tf
@@ -0,0 +1,9 @@
+resource "aws_route_table_association" "pub-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "prv-association" {
+  subnet_id      = aws_subnet.private-subnet.id
+  route_table_id = aws_route_table.private-route-table.id
+}
