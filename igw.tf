68a16458b1bc810bf0941cf8c6899ef797e80c29
Ubuntu <ubuntu@ip-172-31-1-254.ec2.internal>
2025-03-17 12:29:08 +0000

    Moved files into the correct directory

--- igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.capstone.id
-
-  tags = {
-    Name = "igw"
-  }
-}
--- nat.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_nat_gateway" "NAT" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public-subnet.id
-
-  tags = {
-    Name = "NAT"
-  }
-}
