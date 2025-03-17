af519b53eb244176dba943cacfc56a23ebba687b
saicharan-clan <ssaicharan0990@gmail.com>
2025-03-18 02:15:28 +0530

    Update vm.tf

--- terraform files for azure infra/vm.tf
+++ terraform files for azure infra/vm.tf
@@ -1,29 +1,24 @@
-# App Machine
+#App Machine
 
-resource "azurerm_virtual_machine" "App-Machine" {
-  name                  = "App_Machine"
+resource "azurerm_linux_virtual_machine" "app-machine" {
+  name                  = "app-machine"
   location              = azurerm_resource_group.capstone-project.location
   resource_group_name   = azurerm_resource_group.capstone-project.name
-  vm_size               = "Standard B4ms"
-  subnet_id             = azurerm_subnet.public-subnet.id
+  size               = "Standard_B4ms"
+  admin_username = "azureuser"
   network_interface_ids = [azurerm_network_interface.network-interface.id]
-  
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    create_option     = "FromImage"
-    managed           = true
-    os_type           = "Linux"
   }
 
   source_image_reference {
-    id        = "Ubuntu Server 24.04 LTS - x64 Gen2"
     publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "22_04-lts"
+    offer     = "UbuntuServer"
+    sku       = "24_04-lts"
     version   = "latest"
   }
-}
 
   admin_ssh_key {
     username   = "azureuser"
@@ -31,44 +26,37 @@ resource "azurerm_virtual_machine" "App-Machine" {
   }
 
 tags = {
-    environment = "App Machine"
+    environment = "staging"
   }
 }
-
 
 # Tools Machine
 
-resource "azurerm_virtual_machine" "Tools-Machine" {
-  name                  = "Tools_Machine"
+resource "azurerm_linux_virtual_machine" "tools-machine" {
+  name                  = "tools-machine"
   location              = azurerm_resource_group.capstone-project.location
   resource_group_name   = azurerm_resource_group.capstone-project.name
-  vm_size               = "Standard B4ms"
-  subnet_id             = azurerm_subnet.public-subnet.id
+  size               = "Standard_B4ms"
+  admin_username        = "azureuser"
   network_interface_ids = [azurerm_network_interface.network-interface.id]
-  
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    create_option     = "FromImage"
-    managed           = true
-    os_type           = "Linux"
   }
 
   source_image_reference {
-    id        = "Ubuntu Server 24.04 LTS - x64 Gen2"
     publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "22_04-lts"
+    offer     = "UbuntuServer"
+    sku       = "24_04-lts"
     version   = "latest"
   }
-}
 
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("/home/sreemusiri/.ssh/id_rsa.pub")
   }
-
 tags = {
-    environment = "Tools Machine"
+    environment = "staging"
   }
 }
