005f3821dd4787ab3c760690ae16f439b0f44335
saicharan-clan <ssaicharan0990@gmail.com>
2025-03-17 18:18:37 +0530

    Create routetable.tf

---
+++ terraform files for aws infra/routetable.tf
@@ -0,0 +1,29 @@
+#public route table
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.capstone.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+#private route table
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.capstone.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NAT.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
