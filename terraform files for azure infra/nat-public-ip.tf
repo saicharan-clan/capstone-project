8ca8dd08e31c04736809ce0073dadc4d3c167a3e
saicharan-clan <ssaicharan0990@gmail.com>
2025-03-19 22:45:35 +0530

    Update nat-public-ip.tf

--- terraform files for azure infra/nat-public-ip.tf
+++ terraform files for azure infra/nat-public-ip.tf
@@ -8,3 +8,27 @@ resource "azurerm_public_ip" "nat-pub-ip" {
     environment = "Production"
   }
 }
+
+# Public IP for App Machine
+resource "azurerm_public_ip" "app-machine-ip" {
+  name                = "app-machine-ip"
+  resource_group_name = azurerm_resource_group.capstone-project.name
+  location            = azurerm_resource_group.capstone-project.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+# Public IP for Tools Machine
+resource "azurerm_public_ip" "tools-machine-ip" {
+  name                = "tools-machine-ip"
+  resource_group_name = azurerm_resource_group.capstone-project.name
+  location            = azurerm_resource_group.capstone-project.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
