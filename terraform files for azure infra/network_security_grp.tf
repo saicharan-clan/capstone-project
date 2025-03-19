4b97b0797820e6316a7f659cd3fc34b771770752
saicharan-clan <ssaicharan0990@gmail.com>
2025-03-19 22:12:57 +0530

    Update network_security_grp.tf

--- terraform files for azure infra/network_security_grp.tf
+++ terraform files for azure infra/network_security_grp.tf
@@ -1,4 +1,11 @@
-resource "azurerm_network_interface_security_group_association" "nsg-association" {
-  network_interface_id      = azurerm_network_interface.network-interface.id
+# Associate NSG with app-machine NIC
+resource "azurerm_network_interface_security_group_association" "app-machine-nsg-association" {
+  network_interface_id      = azurerm_network_interface.app-machine-nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Associate NSG with tools-machine NIC
+resource "azurerm_network_interface_security_group_association" "tools-machine-nsg-association" {
+  network_interface_id      = azurerm_network_interface.tools-machine-nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
